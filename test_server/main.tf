c5e7a991523c589b6703611a3d75e1fdcded7e3e
AJAY2999 <124439839+AJAY2999@users.noreply.github.com>
2023-04-18 17:16:59 +0530

    Update main.tf

--- test_server/main.tf
+++ test_server/main.tf
@@ -1,35 +1,35 @@
 resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.test-server.id
-  allocation_id = "eipalloc-0604226a57fe8b921"
+  instance_id   = aws_instance.testserver.id
+  allocation_id = "eipalloc-0ec3da46994ebecaf"
 }
-resource "aws_instance" "test-server" {
+resource "aws_instance" "testserver" {
   ami           = "ami-06fc49795bc410a0c" 
   instance_type = "t2.micro" 
-  key_name = "awsmobakey"
-  vpc_security_group_ids= ["sg-08290b52fa64c3be6"]
+  key_name = "awsajay2999"
+  vpc_security_group_ids= ["sg-072ba23abfbf88d71"]
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("./awsmobakey.pem")
+    private_key = file("./awsajay2999.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
     inline = [ "echo 'wait to start instance' "]
   }
   tags = {
-    Name = "test-server"
+    Name = "testserver"
   }
  provisioner "local-exec" {
 
-        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+        command = " echo ${aws_instance.testserver.public_ip} > inventory "
  }
  
  provisioner "local-exec" {
- command = "sudo ansible-playbook /var/lib/jenkins/workspace/BankingandFinanceDomain/test_server/test_bank_playbook.yml "
+ command = "sudo ansible-playbook /var/lib/jenkins/workspace/project2-banking/test_server/ajay_playbook.yml "
   } 
 }
 
-output "test-server_public_ip" {
+output "testserver_public_ip" {
 
   value = aws_eip_association.eip_assoc.public_ip
   
